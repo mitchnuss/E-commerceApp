9132ddd45f6b8d0ab729f877255dea22184dd03e
aaranda14 <aaranda14@my.gcu.edu>
2023-07-05 13:56:27 -0700

    Updated Login PostMapping and Error Handling

--- milestoneCST-339/src/main/java/com/gcu/controller/ApplicationController.java
+++ milestoneCST-339/src/main/java/com/gcu/controller/ApplicationController.java
@@ -1,6 +1,7 @@
 package com.gcu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,9 @@ public class ApplicationController {
         this.productService = productService;
         this.userService = userService;
     }
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/")
     public String home() {
@@ -32,6 +36,28 @@ public class ApplicationController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+    
+    @PostMapping("/login")
+    public String loginUser(@RequestParam("username") String username,
+    						@RequestParam("password") String password,
+    						RedirectAttributes redirectAttributes,
+    						Model model) {
+    try {
+    UserModel user = userService.getUserByUsername(username);
+    if (user != null && passwordEncoder.matches(password, user.getPassword())) {
+    	// Authentication successful
+    	// You can set the user authentication status or perform additional actions here
+    	return "redirect:/products";
+    } else {
+    	// Authentication failed, show error message
+    	model.addAttribute("errorMessage", "Invalid username or password");
+    	return "login";
+    }
+    } catch (IllegalArgumentException e) {
+    	model.addAttribute("errorMessage", "Invalid username or password");
+    	return "login";
+    	}
     }
 
     @GetMapping("/register")
